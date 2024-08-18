44d1655313158ba7ae79a939b7b574b41b5c1da0
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-08-18 20:55:24 +0300

    finished detial page

--- src/app/[locale]/products/[product]/page.jsx
+++ src/app/[locale]/products/[product]/page.jsx
@@ -2,125 +2,148 @@ import ImageOverlaysCenter from "@/components/ImageOverlaysCenter";
 import React from "react";
 import Image from "next/image";
 
-const page = ({ product }) => {
-  // const products = [
-  //   {
-  //     id: 1,
-  //     title: "Rital Chopper 1001",
-  //     imageUrl: "/products/chooper3l.png",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Rital Chopper 1002",
-  //     imageUrl: "/products/chooper6l.png",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Rital Pressure Cooker",
-  //     imageUrl: "/products/cooker2.png",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Rital Blender",
-  //     imageUrl: "/products/Blender2.png",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Rital Air Fryer",
-  //     imageUrl: "/products/Airfryer1.png",
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "Rital Turkish Tea Maker",
-  //     imageUrl: "/products/Teamaker2.png",
-  //   },
-  //   {
-  //     id: 7,
-  //     title: "Rital Vacuum Cleaner",
-  //     imageUrl: "/products/Vacuum1.png",
-  //   },
-  //   {
-  //     id: 8,
-  //     title: "Rital Heater",
-  //     imageUrl: "/products/heater.png",
-  //   },
-  //   {
-  //     id: 9,
-  //     title: "Rital Hand Mixer",
-  //     imageUrl: "/products/Prmixer.png",
-  //   },
-  //   {
-  //     id: 10,
-  //     title: "Rital Water Boiler",
-  //     imageUrl: "/products/Prboiler.png",
-  //   },
-  // ];
-  console.log(product);
+const page = ({ params }) => {
+  const products = [
+    {
+      id: "1",
+      title: "Rital Chopper 1001",
+      imageUrl: "/products/chooper3l.png",
+      detail: [
+        "Power:1000W",
+        "Capacity: 3L",
+        "3 different Speeds",
+        "Glass Container",
+        "Stainless Steel Blades",
+      ],
+    },
+    {
+      id: "2",
+      title: "Rital Chopper 1002",
+      imageUrl: "/products/chooper6l.png",
+      detail: [
+        "Power:1000W",
+        "Capacity: 6L",
+        "3 different Speeds",
+        "Stainless Steel Container",
+        "Stainless Steel Blades",
+      ],
+    },
+    {
+      id: "3",
+      title: "Rital Pressure Cooker",
+      imageUrl: "/products/cooker2.png",
+      detail: [
+        "Power: 1000W",
+        "Capacity: 6L",
+        "Caliber 22cm",
+        "Working Pressure: 0 – 70 kPa",
+        "Limit Pressure: 90 kPa",
+        "Holding Temperature: 60 – 80 Degrees",
+      ],
+    },
+    {
+      id: "4",
+      title: "Rital Blender",
+      imageUrl: "/products/Blender2.png",
+      detail: [
+        "Power:5500W",
+        "Function: Total crushing",
+        "15 different speeds",
+        "Stainless steel blades",
+      ],
+    },
+    {
+      id: "5",
+      title: "Rital Air Fryer",
+      imageUrl: "/products/Airfryer1.png",
+      detail: [
+        "Power: 1700W",
+        "Capacity: 5.6 Liters",
+        "Temperature: 80 – 200 degrees",
+        "Timer: 0 – 60 minutes",
+        "Cord: 1.2 m",
+      ],
+    },
+    {
+      id: "6",
+      title: "Rital Turkish Tea Maker",
+      imageUrl: "/products/Teamaker2.png",
+      detail: [
+        "Power: 1850 – 2200W",
+        "Capacity: 1.7L Kettle & 1L Teapot",
+        "Borosilicate Glass",
+        "Setting Temperature: 40 – 100 Degrees",
+        "Stainless Steel",
+        "Auto shut-off",
+        "Keep warm & Temperature control",
+      ],
+    },
+    {
+      id: "7",
+      title: "Rital Vacuum Cleaner",
+      imageUrl: "/products/Vacuum1.png",
+      detail: [
+        "Power: 3000W",
+        "Capacity: 60L",
+        "Airflow: 120L/min",
+        "Cord: 5 meters",
+        "Hose Length: 1.5m",
+      ],
+    },
+    {
+      id: "8",
+      title: "Rital Heater",
+      imageUrl: "/products/heater.png",
+      detail: [
+        "Power: 1200W",
+        "Flame retardant material",
+        "Overheating protection",
+        "Silent operation",
+        "High quality carbon heating element",
+      ],
+    },
+    {
+      id: "9",
+      title: "Rital Hand Mixer",
+      imageUrl: "/products/Prmixer.png",
+      detail: ["Power: 120W", "Stainless Steel Blades", "Weight: 0.75 kg"],
+    },
+    {
+      id: "10",
+      title: "Rital Water Boiler",
+      imageUrl: "/products/Prboiler.png",
+      detail: ["Power: 120W", "Stainless Steel Blades", "Weight: 0.75 kg"],
+    },
+  ];
+  const product = products.filter((item) => item.id === params.product);
   return (
     <div>
       <ImageOverlaysCenter
         title="Product"
-        des="Rital Pressure Cooker"
+        des={product[0].title}
         imgURL="/product.png"
       />
       <div className="max-w-screen-xl my-4 lg:my-10 mx-auto flex flex-col lg:flex-row items-center justify-center">
         <Image
-          src="/b2.jpg"
+          src={product[0].imageUrl}
           width={300}
           height={300}
-          alt="about"
-          className="rounded-xl mb-4 lg:mb-0"
+          alt="detail"
+          className="rounded-xl w-[400px] lg:w-[800px] h-full mb-4 lg:mb-0 shadow-lg"
         />
         <div className="mx-4 lg:mx-10 w-full">
           <div className="mb-5 w-full">
-            <p className="text-base lg:text-xl text-center mb-3 lg:mb-5 bg-secand text-white p-2">
-              Power: 1000W
-            </p>
-            <p className="text-base lg:text-xl text-center mb-3 lg:mb-5 bg-secand text-white p-2">
-              Capacity: 6L
-            </p>
-            <p className="text-base lg:text-xl text-center mb-3 lg:mb-5 bg-secand text-white p-2">
-              Caliber 22cm
-            </p>
-            <p className="text-base lg:text-xl text-center mb-3 lg:mb-5 bg-secand text-white p-2">
-              Working Pressure: 0 – 70 kPa
-            </p>
-            <p className="text-base lg:text-xl text-center mb-3 lg:mb-5 bg-secand text-white p-2">
-              Limit Pressure: 90 kPa
-            </p>
-            <p className="text-base lg:text-xl text-center mb-3 lg:mb-5 bg-secand text-white p-2">
-              Holding Temperature: 60 – 80 Degrees
-            </p>
+            {product[0].detail.map((item, index) => (
+              <p
+                key={index}
+                className="text-base lg:text-xl mx-3 text-center mb-3 rounded-lg lg:mb-5 bg-secand text-white p-2"
+              >
+                {item}
+              </p>
+            ))}
           </div>
         </div>
       </div>
-      <p className="max-w-screen-xl m-auto my-3 text-justify p-4 lg:p-0">
-        Rital Electrics prides itself on delivering superior products crafted
-        with Italian precision and excellence. Since our establishment in 2021,
-        we have been dedicated to enhancing the comfort, convenience, and
-        efficiency of your daily life through our innovative range of electronic
-        devices. At Rital Electrics, we understand that the heart of every home
-        lies within the kitchen. That is why we have meticulously designed and
-        manufactured a wide array of kitchen appliances to cater to your diverse
-        culinary needs. From state-of-the-art air fryers to sleek coffee
-        machines and blenders, our products combine cutting-edge technology with
-        Italian craftsmanship to elevate your cooking experience. We believe in
-        the power of innovation and continuously strive to introduce new and
-        advanced features in our products. Our team of skilled engineers and
-        designers work tirelessly to bring you the latest advancements in
-        electronic technology, ensuring that our appliances are not only
-        functional Rital Electrics prides itself on delivering superior products
-        crafted with Italian precision and excellence. Since our establishment
-        in 2021, we have been dedicated to enhancing the comfort, convenience,
-        and efficiency of your daily life through our innovative range of
-        electronic devices. At Rital Electrics, we understand that the heart of
-        every home lies within the kitchen. That is why we have meticulously
-        designed and manufactured a wide array of kitchen appliances to cater to
-        your diverse culinary needs. From state-of-the-art air fryers to sleek
-        coffee machines and blenders, our products combine cutting-edge
-        technology with Italian craftsmanship to elevate your cooking
-        experience.
-      </p>
     </div>
   );
 };
